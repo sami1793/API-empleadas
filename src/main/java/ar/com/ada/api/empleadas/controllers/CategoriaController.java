dc01ab0734098f7ef25c6362873c2c8a3b0ae561
sami1793 <sami_17_94@hotmail.com>
2021-06-20 22:13:08 -0300

    Se crean algunos web methods

---
+++ src/main/java/ar/com/ada/api/empleadas/controllers/CategoriaController.java
@@ -0,0 +1,36 @@
+package ar.com.ada.api.empleadas.controllers;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.models.response.GenericResponse;
+import ar.com.ada.api.empleadas.services.CategoriaService;
+
+@RestController
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService service;
+    
+    @PostMapping("/categorias")
+    public ResponseEntity<?> crearCategoria(@RequestBody Categoria categoria){
+        GenericResponse respuesta = new GenericResponse(); 
+        service.crearCategoria(categoria);
+
+        respuesta.isOk=true;//porque si llegue hasta acá es porque esta todo bien
+        respuesta.id= categoria.getCategoria_Id();
+        respuesta.message= "La categoria fue creada con éxito";
+
+        return ResponseEntity.ok(respuesta);
+    }
+
+    @GetMapping("/categorias")
+    public ResponseEntity<List<Categoria>> traerCategorias(){
+        return ResponseEntity.ok(service.traerCategorias());
+    }
+}
---
+++ src/main/java/ar/com/ada/api/empleadas/services/CategoriaService.java
@@ -0,0 +1,5 @@
+package ar.com.ada.api.empleadas.services;
+
+public class CategoriaService {
+    
+}
