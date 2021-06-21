018e272132dc876cec5e7c3958a50939f99f1ee1
sami1793 <sami_17_94@hotmail.com>
2021-06-20 21:01:00 -0300

    Se crearon notaciones para persistir en la db.

--- src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
@@ -1,13 +1,27 @@
 package ar.com.ada.api.empleadas.entities;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.Generated;
+import javax.persistence.*;
+@Entity
+@Table(name="categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="categoria_id")
     private Integer categoria_Id;
 
     private String nombre;
 
+    @Column(name="sueldo_base")
     private BigDecimal sueldoBase;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Empleada> empleadas= new ArrayList<>();
 
     public Integer getCategoria_Id() {
         return categoria_Id;
